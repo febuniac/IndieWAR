d4c1df4821011ae8d88fd07f3c3e5c12f1cf68f2
febuniac <febuniac@gmail.com>
2017-11-27 19:36:29 -0200

    Menu

---
+++ Indie WAR/Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonEvent : MonoBehaviour {
+	public Button yourButton;
+	// Use this for initialization
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+		
+	}
+	
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+	}
+}
+
